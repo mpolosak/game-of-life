9c8c3308bad3d8502c5c6a4993aa2dbd9b74bdef
Michał Połosak <mpolosak@op.pl>
2021-07-28 18:21:25 +0200

    Correct appearanceconfig.hpp to compile

--- src/appearanceconfig.hpp
+++ src/appearanceconfig.hpp
@@ -1,13 +1,16 @@
+#include<iostream>
+#include<string>
+
 class AppearanceConfig
 {
 public:
     void setColours();
-    std::string getColours(){return coloursString};
+    std::string getColours(){return coloursString;};
     unsigned short liveCellColour;
     unsigned short deathCellColour;
     unsigned short backgroundColour;
 private:
     std::string coloursString;
-}
+};
 
-std::ostream& operator<<(std::ostream& os, cont AppearanceConfig& config);
+std::ostream& operator<<(std::ostream& os, const AppearanceConfig& config);
